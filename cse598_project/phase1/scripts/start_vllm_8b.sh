8ab41702279a8e998d686deb8be6cc230f8013bf
V Shravan Krishna <shravankrishnav@gmail.com>
2026-02-10 15:52:55 -0700

    chore: revert to A100/CUDA setup (remove Gaudi support)

--- cse598_project/phase1/scripts/start_vllm_8b.sh
+++ cse598_project/phase1/scripts/start_vllm_8b.sh
@@ -27,6 +27,5 @@ $PYTHON_EXEC -m vllm.entrypoints.openai.api_server \
     --dtype float16 \
     --max-model-len 40960 \
     --tensor-parallel-size 1 \
-    --block-size 128 \
     --gpu-memory-utilization 0.9 \
-    --swap-space 64
+    --swap-space 16
