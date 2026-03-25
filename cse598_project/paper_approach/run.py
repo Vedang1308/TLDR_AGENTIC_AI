58f570339176a297bc2d8bff767f1d28cf7013e0
Vedang <vedangavaghade01@gmail.com>
2026-03-25 01:31:45 -0700

    Sync path and import fixes from vedang-gaudi

--- cse598_project/paper_approach/run.py
+++ cse598_project/paper_approach/run.py
@@ -31,5 +31,5 @@ import cse598_project.phase3.tau_bench.run as original_run_module
 original_run_module.agent_factory = paper_agent_factory
 
 if __name__ == "__main__":
-    from cse598_project.phase3.tau_bench.run import main
+    from cse598_project.phase3.run import main
     main()
